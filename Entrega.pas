ee32ab8ce1baef981fbbd292c9f7410b1b169091
Román Girard <Romn Girard>
2016-10-25 20:40:59 -0300

    Segunda entrega

--- Entrega.pas
+++ Entrega.pas
@@ -1,7 +1,7 @@
-program entrega1;
+program Entrega;
 
-uses crt;
-
+uses crt, math;
+      
 type registro = record
                     Sepal_Length : real;
                     Sepal_Width : real;
@@ -10,16 +10,121 @@ type registro = record
                     Species : string;
                 end;
 
+          tipo = record      
+                    Sepal_Length : real;
+                    Sepal_Width : real;
+                    Petal_Length :real;
+                    Petal_Width : real;
+                    Species : string;
+                    Distancia : real;
+                  end;
+
+                  
+
 var       datos : file of registro;
           r : registro;
           i , j : integer;
           Muestras : array [1..120] of registro;
           Testeo: array [1..30] of registro;
+          Distancias : array [1..120] of real;
           Ver: integer;
           Ss : integer;
           Vir : integer;
           s : integer;
           c : integer;
+          WP : real;
+          LP : real;
+         // D : real;
+          const k=3;
+
+          
+    
+         
+          function distancia(x1 : real; y1 : real; x2 : real; y2: real) : real;
+              begin
+                   distancia := sqrt(power(x1 - x2,2) + power(y1 - y2,2));                  
+              end;
+
+
+          function tipos(x1 : real; y1 : real) : string;
+            var Aux_distancia : real;
+              aux_datos : registro;
+                     Ver: integer;
+          Ss : integer;
+          Vir : integer;
+            begin
+              Ver := 0;
+              Ss := 0;
+              Vir := 0;
+                for i := 1 to 120 do
+                  begin
+                      Distancias[i]  := distancia(x1,y1, muestras[i].petal_length, muestras[i].Petal_Width);
+                  end;    
+                for i := 1 to 119 do
+                begin
+                      for j := i+1 to 120 do
+                      begin
+                                if Distancias[i] > Distancias[j] then
+                                  begin
+                                      Aux_datos := Muestras[i]; 
+                                      Muestras[i] := Muestras[j];
+                                      Muestras[j] := Aux_datos;
+
+
+                                      Aux_distancia := Distancias[i];
+                                      Distancias[i] := Distancias[j];
+                                      Distancias[j] := Aux_distancia; 
+                                  end;
+
+                      end;
+                 end;
+                                 
+                for i := 1 to k do
+                begin
+                              
+                                if Muestras[i].Species = 'versicolor' then
+                                begin
+                                    Ver := ver + 1;
+                                end
+                                else
+                                begin
+                                    if Muestras[i].Species = 'setosa' then
+                                    begin
+                                                  Ss := Ss + 1;
+                                    end
+                                    else
+                                    begin
+                                                  vir := vir + 1;
+                                    end;
+                                 end; 
+                                 end;
+
+                                 if ver > Ss then
+                                 begin
+                                    if ver > vir then
+                                    begin
+                                            tipos := 'versicolor';
+                                    end
+                                    else
+                                    begin
+                                            tipos := 'virginica';
+                                    end;
+                                 end
+                                 else
+                                 begin
+                                    if ss > vir then
+                                    begin
+                                        tipos := 'setosa';
+                                    end
+                                    else
+                                    begin
+                                            tipos := 'virginica';
+                                    end;
+                                 end;
+                 end;
+                        
+                
+          
 
 begin
 
@@ -82,16 +187,6 @@ while not eof(datos) do
 
 close(datos);
 
-for i := 1 to 120 do
-  begin
-      writeln(i, ' ',Muestras[i].Species);
-      readkey();  
-  end;
-
-for j := 1 to 30 do
-  begin
-      writeln(j, '  ', Testeo[j].Species);
-      readkey();     
-  end;
+//writeln(tipos(10,10));
 
 end.
